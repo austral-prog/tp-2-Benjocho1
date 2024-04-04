76f8046b42488f87f469b82a4c830db2776d9942
Benjocho (Benjamin) <102769407+Benjocho1@users.noreply.github.com>
2024-04-04 18:38:18 -0300

    ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,14 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    space = " "
+    
+    print(f'{first_name}  {space}  {last_name}'.lower())
+    
+    print(f'{first_name}  {space}  {last_name}'.capitalize())
+    
+    print(f'{first_name}  {space}  {last_name}'.upper())
+    
+    print(f'\t{first_name}  {space}  {last_name}'.lower())
+    
+    
